7e67ac27b919794b29e58b34fb3e932832966339
Millie Clare <Millie_Clare@outlook.com>
2024-05-19 18:57:42 +0100

    added Footer section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Form from "./Components/Form";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Form />
+      <Footer />
     </div>
   );
 }
